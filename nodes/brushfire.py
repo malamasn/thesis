fd3298981be6bc960fe913c9e42560b371f178a5
Nikolas Malamas <malamasn@hotmail.com>
2019-03-03 17:44:36 +0200

    Add brushfire file

---
+++ nodes/brushfire.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+import math
+from collections import deque
+
+# Class with brushfire functions
+class Brushfire:
+    # Brushfire starting from obstacles
+    def obstacleBrushfire(self, ogm):
+        width = ogm.shape[0]
+        height = ogm.shape[1]
+        # Free space == 0 and obstacles == 1
+        brushfire = np.full(ogm.shape, 0)
+        brushfire[(ogm < 66) & (ogm != -1)] = 1
+
+        # Queue obstacles' neighbors
+        queue = deque()
+        for x in range(width):   # MAKE IT FASTER
+            for y in range(height):
+                # neighbor checks around ogm[x][y] for obstacles
+                neighbor = np.any(ogm[x-1:x+2][y-1:y+2] == 1)
+                if not ogm[x][y] and neighbor:
+                    queue.append((x, y))
+
+        # For all zero pixels do
+        while(queue != []):
+            x, y = queue.popleft()
+            # If already brushfired skip
+            if brushfire[x][y]:
+                continue
+            # Add non brushfired neighbors of pixel[x][y]
+            brush_value = np.inf
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    # Boundary check
+                    if x+i < 0 or x+i > width or y+j < 0 or y+j > height or gvd[x+i][y+j] == 0:
+                        continue
+                    if i == 0 and j == 0:
+                        continue
+                    if not brushfire[x+i][y+j]:
+                        queue.append((x+i,y+j))
+                    else:
+                        if brushfire[x+i][y+j] < brush_value:
+                            brush_value = brushfire[x+i][y+j]
+            # Brushfire value comes from closest obstacle
+            brushfire[x][y] = brush_value + 1
+        return brushfire
