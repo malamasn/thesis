10833712afc1b318c239ba0bd2089e46d734d884
Nikolas Malamas <malamasn@hotmail.com>
2019-03-06 20:40:56 +0200

    Make brushfire run, correct indexing bug and delete debugging lines

--- nodes/brushfire.py
+++ nodes/brushfire.py
@@ -10,22 +10,22 @@ class Brushfire:
     def obstacleBrushfire(self, ogm):
         width = ogm.shape[0]
         height = ogm.shape[1]
-        print(width, height, 'w x h')
+
         # Free space == 0, obstacles == 1, unknown == -1 at brushfire field
         brushfire = np.zeros(ogm.shape, np.dtype('int8'))
         brushfire[ogm > 49] = 1
         brushfire[ogm == -1] = -1
-        print(len(brushfire[brushfire==-1]), -1)
-        print(len(brushfire[brushfire!=-1]), ' not -1')
+
         rospy.loginfo("Brushfire initialized.")
         # Queue obstacles' neighbors
         index = np.where(brushfire == 1)
         queue = zip(index[0], index[1])
-        print(len(queue))   # DEBUG
+
         brush_value = 1
         expand = True
         # For all zero pixels do
         while expand:
+
             expand = False
             brush_value += 1
             next_queue = []
@@ -34,16 +34,16 @@ class Brushfire:
                 for i in range(-1,2):
                     for j in range(-1,2):
                         # Boundary check
-                        if x+i < 0 or x+i > width or y+j < 0 or y+j > height:
+                        if x+i < 0 or x+i > width-1 or y+j < 0 or y+j > height-1:
                             continue
                         if i == 0 and j == 0:
                             continue
                         if brushfire[x+i][y+j] == 0:
                             next_queue.append((x+i,y+j))
-                            brushfire[x+i][y+i] = brush_value
+                            brushfire[x+i][y+j] = brush_value
                             expand = True
-            queue = next_queue
-            print(len(queue), brush_value)  # DEBUG
+            # Keep every value only once
+            queue = list(set(next_queue))
 
         rospy.loginfo("Brushfire done!")
         return brushfire
@@ -72,7 +72,7 @@ class Brushfire:
                 for i in range(-1,2):
                     for j in range(-1,2):
                         # Boundary check
-                        if x+i < 0 or x+i > width or y+j < 0 or y+j > height:
+                        if x+i < 0 or x+i > width-1 or y+j < 0 or y+j > height-1:
                             continue
                         if i == 0 and j == 0:
                             continue
@@ -83,7 +83,7 @@ class Brushfire:
                                 found += 1
                             else:
                                 next.append((x+i,y+j))
-                                brushfire[x+i][y+i] = brush_value
+                                brushfire[x+i][y+j] = brush_value
             current = next
             next = []
 
