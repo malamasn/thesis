a06025de15f929f0041c16df70479d85d9e71dce
Nikolas Malamas <malamasn@hotmail.com>
2019-03-22 11:49:08 +0200

    Use closestObstacles and their line to find occupancy % to find doors

--- nodes/topology.py
+++ nodes/topology.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import division
 import rospy
 import numpy as np
 import math
@@ -49,8 +50,7 @@ class Topology:
 
             # Add points with 1 or 3 neighbors
             if count_neighbors >= 4 or count_neighbors == 2:
-                # nodes.append((x,y))
-                pass
+                nodes.append((x,y))
             # For 2-neighbor points check with more detail
             elif count_neighbors == 3:
                 notGood = False
@@ -108,7 +108,7 @@ class Topology:
 
                 # Add medium of line as door node
                 temp.sort()
-                index = len(temp)/2
+                index = len(temp)//2
                 _x, _y = temp[index]
                 if not gvd[_x,_y]:
                     rospy.loginfo('ERROR! Estimated door not in gvd, (x,y):', _x,_y)
@@ -135,7 +135,7 @@ class Topology:
         return nodes
 
     # Discard nodes that don't represent a door
-    def findDoorNodes(self, nodes, allNodes, gvd, brushfire):
+    def findDoorNodes(self, nodes, allNodes, ogm, gvd, brushfire, brushfire_instance):
         doorNodes = []
         width = gvd.shape[0]
         height = gvd.shape[1]
@@ -145,9 +145,50 @@ class Topology:
             node = nodes.pop()
             x = node[0]
             y = node[1]
-            # Nodes far from obstacles usually are not obstacles
-            if brushfire[node] <= 25:   # MAYBE 30 ?
-                doorNodes.append(node)
+
+            # print("candidate door node ", node) # DEBUG:
+            ob = brushfire_instance.closestObstacleBrushfire(node, ogm)
+            # Doors have always just 2 obstacle points
+            if len(ob) != 2:
+                continue
+
+            # Find obstacle points' line
+            obs_x = [ob[0][0], ob[1][0]]
+            obs_y = [ob[0][1], ob[1][1]]
+            # print('obs_x', obs_x, 'obs_y', obs_y)
+            if np.abs(obs_x[0] - obs_x[1]) <= 3:  # a -> inf
+                # print('a->inf') # DEBUG:
+                min_y = np.min(obs_y)
+                max_y = np.max(obs_y)
+                mean_x = int(np.mean(obs_x))
+                index = np.where(ogm[mean_x-3:mean_x+4, min_y-50:max_y+51] > 49)
+                line_points = len(index[0])
+                all_points =  (max_y+51-min_y+50) * (4+3)
+            elif np.abs(obs_y[0] - obs_y[1]) <= 3: # a -> 0
+                # print('a->0')   # DEBUG:
+                min_x = np.min(obs_x)
+                max_x = np.max(obs_x)
+                mean_y = int(np.mean(obs_y))
+                index = np.where(ogm[min_x-50:max_x+51, mean_y-3:mean_y+4] > 49)
+                line_points = len(index[0])
+                all_points =  (max_x+51-min_x+50) * (4+3)
+            else:
+                # print('normal line')    # DEBUG:
+                line_coeffs = np.polyfit(obs_x, obs_y, 1)
+                line_points = 0
+                all_points = 0
+                min_x = np.min(obs_x)
+                max_x = np.max(obs_x)
+                for xx in range(min_x-50, max_x+50):
+                    yy = int(xx * line_coeffs[0] + line_coeffs[1])
+                    index = np.where(ogm[xx, yy-3:yy+4] > 49)
+                    line_points += len(index[0])
+                    all_points += (4+3)
+
+            perc = line_points / all_points
+            # print("obstacles", ob, 'line', line_points, 'all', all_points, 'perc', perc)
+            if perc >= 0.15:  # FIND RIGHT THRESHOLD
+                doorNodes.append((x,y))
         return doorNodes
 
     # Clustering of nodes to rooms with labels
