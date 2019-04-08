ef3ee208192cf51c0c311f4cfdb60b60c355822d
Nikolas Malamas <malamasn@hotmail.com>
2019-04-08 14:00:24 +0300

    Make cffi scripts faster using numpy instead of nested for loops

--- nodes/utilities.py
+++ nodes/utilities.py
@@ -26,9 +26,7 @@ class Cffi:
             yi[i] = ffi.cast("int *", y[i].ctypes.data)
 
         br_c = lib.obstacleBrushfire(xi, yi, len(x), len(x[0]))
-        for i in range(ogm.shape[0]):
-            for j in range(ogm.shape[1]):
-                brushfire[i][j] = yi[i][j]
+        brushfire[0:,0:] = np.array(y)
         return brushfire
 
     @staticmethod
@@ -45,16 +43,11 @@ class Cffi:
         for i in range(len(brushfire_x)):
             brushfire_c[i] = ffi.cast("int *", brushfire_x[i].ctypes.data)
 
-        # neighbor_nodes = ffi.new("int **")
         neighbor_nodes = []
         br_c = lib.gvdNeighborBrushfire(brushfire_c, len(brushfire_x), len(brushfire_x[0]))
-        for node in nodes:
-            i = node[0]
-            j = node[1]
-            if brushfire_c[i][j] == -2:
-                neighbor_nodes.append((i,j))
-        neighbor_nodes = list(set(neighbor_nodes))
-        print('start', start, 'nn', neighbor_nodes)
+        brushfire[:] = np.array(brushfire_x)
+        index = np.where(brushfire == -2)
+        neighbor_nodes = zip(index[0], index[1])
         return neighbor_nodes
 
     @staticmethod
@@ -79,50 +72,30 @@ class Cffi:
 
         brushfire_split = brushfire.copy()
         brushfire_split[nn[0]] = 3
-        brushfire_x = [np.array(v, dtype='int32') for v in brushfire_split]
-        brushfire_c_1 = ffi.new(("int* [%d]") % (len(brushfire_x)))
-        for i in range(len(brushfire_x)):
-            brushfire_c_1[i] = ffi.cast("int *", brushfire_x[i].ctypes.data)
-        print('min 1 before', np.min(brushfire_split), 'max_1 before', np.max(brushfire_split))
+        brushfire_x_1 = [np.array(v, dtype='int32') for v in brushfire_split]
+        brushfire_c_1 = ffi.new(("int* [%d]") % (len(brushfire_x_1)))
+        for i in range(len(brushfire_x_1)):
+            brushfire_c_1[i] = ffi.cast("int *", brushfire_x_1[i].ctypes.data)
 
         brushfire_split = brushfire.copy()
         brushfire_split[nn[1]] = 3
-        brushfire_x = [np.array(v, dtype='int32') for v in brushfire_split]
-        brushfire_c_2 = ffi.new(("int* [%d]") % (len(brushfire_x)))
-        for i in range(len(brushfire_x)):
-            brushfire_c_2[i] = ffi.cast("int *", brushfire_x[i].ctypes.data)
-        print('min 2 before', np.min(brushfire_split), 'max_2 before', np.max(brushfire_split))
-        # neighbor_nodes_first = ffi.new("int **")
-        # neighbor_nodes_second = ffi.new("int **")
+        brushfire_x_2 = [np.array(v, dtype='int32') for v in brushfire_split]
+        brushfire_c_2 = ffi.new(("int* [%d]") % (len(brushfire_x_2)))
+        for i in range(len(brushfire_x_2)):
+            brushfire_c_2[i] = ffi.cast("int *", brushfire_x_2[i].ctypes.data)
 
-        br_c = lib.gvdNeighborSplitBrushfire(brushfire_c_1, brushfire_c_2, len(brushfire_x), len(brushfire_x[0]))
-        # print('nn_first', neighbor_nodes_first, 'nn_second', neighbor_nodes_second)
-        # neighbor_nodes = np.array([neighbor_nodes_first], [neighbor_nodes_second])
+        br_c = lib.gvdNeighborSplitBrushfire(brushfire_c_1, brushfire_c_2, len(brushfire_x_1), len(brushfire_x_1[0]))
+
         br_1 = np.zeros(gvd.shape)
-        for i in range(len(brushfire_x)):
-            for j in range(len(brushfire_x[0])):
-                br_1[i][j] = brushfire_c_1[i][j]
+        br_1[:] = np.array(brushfire_x_1)
         br_2 = np.zeros(gvd.shape)
-        for i in range(len(brushfire_x)):
-            for j in range(len(brushfire_x[0])):
-                br_2[i][j] = brushfire_c_2[i][j]
-        print('min 1', np.min(br_1), 'max_1', np.max(br_1), 'min 2', np.min(br_2), 'max_2', np.max(br_2))
+        br_2[:] = np.array(brushfire_x_2)
+
         neighbor_nodes = []
-        temp = []
-        for node in nodes:
-            i = node[0]
-            j = node[1]
-            if brushfire_c_1[i][j] == -2:
-                temp.append((i,j))
-        temp = list(set(temp))
+        index = np.where(br_1 == -2)
+        temp = zip(index[0], index[1])
         neighbor_nodes.append(temp)
-        temp = []
-        for node in nodes:
-            i = node[0]
-            j = node[1]
-            if brushfire_c_2[i][j] == -2:
-                temp.append((i,j))
-        temp = list(set(temp))
+        index = np.where(br_2 == -2)
+        temp = zip(index[0], index[1])
         neighbor_nodes.append(temp)
-        print('start', start, 'nn', neighbor_nodes)
         return neighbor_nodes
