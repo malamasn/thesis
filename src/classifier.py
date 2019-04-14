ee99386f01de1ef15fd80a863528f89a0fa3cb43
Nikolas Malamas <malamasn@hotmail.com>
2019-04-14 21:49:33 +0300

    Change classifier script

--- src/classifier.py
+++ src/classifier.py
@@ -6,23 +6,24 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.externals import joblib
 
 # Read dataset
-data = pd.read_csv("~/catkin_ws/src/room_classification/datasets/no_dist_reev/room_feature_dataset_no_dist_reev.csv")
+data = pd.read_csv("~/catkin_ws/src/room_classification/datasets/last_data.csv")
 # Split attributes and labels
 x = data.drop('Class', axis=1)
 y = data['Class']
 # Split training and testing
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.3)
+# print(set(y_train))
 # Train model
-model = SVC(kernel='linear')
+model = SVC(kernel='poly')
 model.fit(x_train, y_train)
-# Predict
+# # Predict
 y_pred = model.predict(x_test)
 print(confusion_matrix(y_test, y_pred))
 
 # Save model
-filename = 'room_classifier.sav'
+filename = 'temp.sav'
 joblib.dump(model, filename)
 # # Reevaluate saved model
 # model2 = joblib.load(filename)
-# results = model2.predict(x_test)
-# print(confusion_matrix(y_test, results))
+# results = model2.predict(x)
+# print(confusion_matrix(y, results))
