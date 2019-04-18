ecc3ed04d92a5acf45b22ddbfea9cb2d1df20cba
Nikolas Malamas <malamasn@hotmail.com>
2019-04-18 17:40:00 +0300

    Train models with last_data_no_resolution and keep temp_12 as room_classifier

--- src/classifier.py
+++ src/classifier.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.externals import joblib
 
 # Read dataset
-data = pd.read_csv("~/catkin_ws/src/room_classification/datasets/last_data.csv")
+data = pd.read_csv("~/catkin_ws/src/room_classification/datasets/last_data_no_resolution.csv")
 # Split attributes and labels
 x = data.drop('Class', axis=1)
 y = data['Class']
@@ -19,9 +19,10 @@ model.fit(x_train, y_train)
 # # Predict
 y_pred = model.predict(x_test)
 print(confusion_matrix(y_test, y_pred))
+print(accuracy_score(y_pred, y_test))
 
 # Save model
-filename = 'temp.sav'
+filename = 'temp_12.sav'
 joblib.dump(model, filename)
 # # Reevaluate saved model
 # model2 = joblib.load(filename)
