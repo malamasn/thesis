9140e5930931e0d0057ead0cd1200fccb6ea9fe2
Nikolas Malamas <malamasn@hotmail.com>
2019-04-25 16:10:31 +0300

    Add empty navigation.py

---
+++ nodes/navigation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import rospy, json
+import numpy as np
+import time
+from dijkstar import Graph, find_path
+
+
+
+
+class Navigation:
+
+    def __init__(self):
+        pass
+
+    def server_start(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    node = Navigation()
+    node.server_start()
