f74d987ba067bc2730d51cd35c82140e6d63e3f8
Nikolas Malamas <malamasn@hotmail.com>
2019-04-25 16:50:59 +0300

    Add callback to read robot's current pose in navigation.py

--- nodes/navigation.py
+++ nodes/navigation.py
@@ -4,34 +4,41 @@ import numpy as np
 import time
 from dijkstar import Graph, find_path
 
-
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose, Point
+from visualization_msgs.msg import Marker
 
 
 class Navigation:
 
     def __init__(self):
-        self.routing = Routing()
+        # self.routing = Routing()
         self.resolution = 0.05
-        self.ogm_topic = '/map'
-        self.ogm = 0
-        self.ogm_raw = 0
-        self.ogm_width = 0
-        self.ogm_height = 0
-        self.ogm_header = 0
-        self.gvd = 0
-        self.brush = 0
+
+        self.current_pose = Pose()
+
         self.nodes = []
         self.door_nodes = []
         self.rooms = []
         self.room_doors = []
         self.room_type = []
         self.room_sequence = []
+
         self.node_publisher = rospy.Publisher('/nodes', Marker, queue_size = 100)
         self.door_node_pub = rospy.Publisher('/nodes/doors', Marker, queue_size = 100)
         self.room_node_pub = rospy.Publisher('/nodes/rooms', Marker, queue_size = 100)
+        self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback)
 
+
+    # Cb to read robot's pose
+    def odom_callback(self, msg):
+        self.current_pose =  msg.pose.pose
+        return
 
     def server_start(self):
+        rospy.init_node('navigation')
+        rospy.loginfo('navigation node initialized.')
+
         # Load nodes from json file
         map_name = rospy.get_param('map_name')
         filename = '/home/mal/catkin_ws/src/topology_finder/data/' + map_name +'.json'
