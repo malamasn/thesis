93a5e645380bf6c12a402eae5760f6953829b3ac
Nikolas Malamas <malamasn@hotmail.com>
2019-04-25 16:17:08 +0300

    Read json data in navigation.py

--- nodes/navigation.py
+++ nodes/navigation.py
@@ -10,10 +10,43 @@ from dijkstar import Graph, find_path
 class Navigation:
 
     def __init__(self):
-        pass
+        self.routing = Routing()
+        self.resolution = 0.05
+        self.ogm_topic = '/map'
+        self.ogm = 0
+        self.ogm_raw = 0
+        self.ogm_width = 0
+        self.ogm_height = 0
+        self.ogm_header = 0
+        self.gvd = 0
+        self.brush = 0
+        self.nodes = []
+        self.door_nodes = []
+        self.rooms = []
+        self.room_doors = []
+        self.room_type = []
+        self.room_sequence = []
+        self.node_publisher = rospy.Publisher('/nodes', Marker, queue_size = 100)
+        self.door_node_pub = rospy.Publisher('/nodes/doors', Marker, queue_size = 100)
+        self.room_node_pub = rospy.Publisher('/nodes/rooms', Marker, queue_size = 100)
+
 
     def server_start(self):
-        pass
+        # Load nodes from json file
+        map_name = rospy.get_param('map_name')
+        filename = '/home/mal/catkin_ws/src/topology_finder/data/' + map_name +'.json'
+        with open(filename, 'r') as read_file:
+                data = json.load(read_file)
+
+        self.nodes = data['nodes']
+        self.door_nodes = data['doors']
+        self.rooms = data['rooms']
+        self.room_doors = data['roomDoors']
+        self.room_type = data['roomType']
+        self.room_sequence = data['room_sequence']
+
+
+        return
 
 
 
