ba9c07026e2384aef1e2be519e4eb400b41f31c6
Nikolas Malamas <malamasn@hotmail.com>
2019-05-02 20:44:28 +0300

    Update coverage ogm for entire circle of current robot pose

--- nodes/coverage.py
+++ nodes/coverage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
+from math import hypot
 
 from nav_msgs.msg import Odometry, OccupancyGrid
 from geometry_msgs.msg import Pose
@@ -22,7 +23,7 @@ class Coverage:
         self.sensor_direction = rospy.get_param('sensor_direction')
         self.sensor_fov = rospy.get_param('fov')
         self.sensor_range = rospy.get_param('range')
-        self.sensor_hape = rospy.get_param('shape')
+        self.sensor_shape = rospy.get_param('shape')
         self.sensor_reliability = rospy.get_param('reliability')
 
         # Load map's translation
@@ -51,7 +52,12 @@ class Coverage:
         rospy.Subscriber(self.ogm_topic, OccupancyGrid, self.read_ogm)
 
         self.current_pose = Pose()
-        rospy.Subscriber('/odom', Odometry, self.odom_callback)
+        # robot_pose is current_pose in map's frame (aka in pixels)
+        self.robot_pose = {}
+        self.robot_pose['x'] = 0
+        self.robot_pose['y'] = 0
+        self.robot_pose['th'] = 0
+
 
         self.coverage_pub = rospy.Publisher(self.coverage_topic, \
             OccupancyGrid, queue_size = 10)
@@ -69,6 +75,54 @@ class Coverage:
     # Cb to read robot's pose
     def odom_callback(self, msg):
         self.current_pose =  msg.pose.pose
+
+        # Calculate current x,y in map's frame (aka in pixels)
+        self.robot_pose['x'] = (self.current_pose.position.x - self.origin['x'])/self.resolution
+        self.robot_pose['y'] = (self.current_pose.position.y - self.origin['y'])/self.resolution
+
+        # Getting the Euler angles
+        angles = tf.transformations.euler_from_quaternion(rotation)
+        self.robot_pose['th'] = angles[2]   # yaw
+
+        cover_length = int(self.sensor_range / self.resolution)
+        xx = self.robot_pose['x']
+        yy = self.robot_pose['y']
+
+        if self.sensor_shape == 'rectangular':
+            for i in range(-cover_length, cover_length):
+                for j in range(-cover_length, cover_length):
+                    x = int(xx + i)
+                    y = int(yy + j)
+                    if self.ogm[x, y] > 49 or self.ogm[x, y] == -1:
+                        continue
+                    # TODO: Use fov!!!
+                    
+                    self.coverage[x, y] = 100
+                    index = int((xx + i) + self.ogm_info.width * (yy + j))
+                    self.coverage_ogm.data[index] = 100
+
+        elif self.sensor_shape == 'circular':
+            for i in range(-cover_length, cover_length):
+                for j in range(-cover_length, cover_length):
+                    x = int(xx + i)
+                    y = int(yy + j)
+                    if self.ogm[x, y] > 49 or self.ogm[x, y] == -1:
+                        continue
+                    # Check if point inside cover circle
+                    if hypot(xx-x, yy-y) > cover_length:
+                        continue
+                    # TODO: Use fov!!!
+
+                    self.coverage[x, y] = 100
+                    index = int((xx + i) + self.ogm_info.width * (yy + j))
+                    self.coverage_ogm.data[index] = 100
+
+
+
+        self.coverage_publisher.publish(self.coverage_ogm)
+
+
+
 
 
         return
